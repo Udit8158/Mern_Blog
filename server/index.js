91486acd20f1cb02465c9dba17e50a9ad76c304e
Udit8158 <kunduudit8158@gmail.com>
2023-01-08 18:47:05 +0530

    working on with upload pic

--- server/index.js
+++ server/index.js
@@ -3,14 +3,26 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 import authRoute from "./routes/auth.js";
 import userRoute from "./routes/user.js";
 import blogRoute from "./routes/blog.js";
 
-// Initialize app
+// Initialize app and port
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+// Initialize storage and upload settings
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+const upload = multer({ storage: storage });
 
 // Connect to mongoose
 mongoose.set("strictQuery", false);
@@ -23,6 +35,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+app.post("/api/v1/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Upload successfuly");
+});
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/blogs", blogRoute);
