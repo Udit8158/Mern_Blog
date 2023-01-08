af884bf942eed675ccb308cd58ca39726e96fba4
Udit8158 <kunduudit8158@gmail.com>
2023-01-08 18:52:48 +0530

    fix in get blogs

--- server/controllers/blog/getAllBlogs.js
+++ server/controllers/blog/getAllBlogs.js
@@ -2,6 +2,7 @@ import Blog from "../../models/Blog.js";
 
 const getAllBlogs = async (req, res) => {
   const catName = req.query.cat;
+  const userName = req.query.usr;
 
   // Check search only for a certain category
   if (catName) {
@@ -11,6 +12,16 @@ const getAllBlogs = async (req, res) => {
       return res
         .status(404)
         .json({ message: "No Blogs found! in this catagory" });
+
+    return res.status(200).json(filteredBlogs);
+  }
+
+  // check search only any user's blog
+  if (userName) {
+    // Filter by user
+    const filteredBlogs = await Blog.find({ author: userName });
+    if (filteredBlogs.length === 0)
+      return res.status(404).json({ message: "No blog found of this user." });
 
     return res.status(200).json(filteredBlogs);
   }
