621328f76afed7737a0719e9ac610605a68f8376
Udit8158 <kunduudit8158@gmail.com>
2023-01-12 13:30:19 +0530

    make routes protected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import Auth from "./pages/Auth";
 import BlogDetails from "./pages/BlogDetails";
@@ -8,14 +8,15 @@ import Home from "./pages/Home";
 import UserProfile from "./pages/UserProfile";
 
 const App = () => {
-  // const {} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/create-blog" element={<CreateBlog />} />
-      <Route path="/user-profile" element={<UserProfile />} />
-      <Route path="/blogs/:blogId" element={<BlogDetails />} />
+      {user && <Route path="/" element={<Home />} />}
+      {!user && <Route path="/auth" element={<Auth />} />}
+      {user && <Route path="/create-blog" element={<CreateBlog />} />}
+      {user && <Route path="/user-profile" element={<UserProfile />} />}
+      {user && <Route path="/blogs/:blogId" element={<BlogDetails />} />}
+      <Route path="/*" element={<Navigate to={user ? "/" : "/auth"} />} />
     </Routes>
   );
 };
